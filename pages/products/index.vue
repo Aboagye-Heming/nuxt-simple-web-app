dc112692395cc9948507a061dd4f1ee536097173
Aboagye=Heming <hemingkwabena@gmail.com>
2024-07-15 18:43:01 +0000

    added pages and route parameters

---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Products</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia
+      culpa, sapiente ducimus possimus expedita quae sed delectus debitis
+      repellat sequi! A similique natus veniam perferendis neque unde ea hic.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
