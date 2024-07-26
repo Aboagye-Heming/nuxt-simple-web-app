b442cc75cf4ccf28b12a3aed4a6ebb5ff02e90ee
Aboagye=Heming <hemingkwabena@gmail.com>
2024-07-26 12:28:19 +0000

    added a product details

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
-    <p>{{ productDetails.title }}</p>
-    <p>{{ productDetails.price }}</p>
-    <p>{{ productDetails.id }}</p>
+  <div class="card">
+    <div class="grid grid-cols-2 gap-10">
+      <div class="p-7">
+        <img
+          :src="productDetails.image"
+          alt="product Img"
+          class="mx-auto my-7"
+        />
+      </div>
+      <div class="p-7">
+        <h2 class="text-4xl my-7">
+          {{ productDetails.title }}
+        </h2>
+        <p class="text-xl my-7">Price- ${{ productDetails.price }} </p>
+        <h3 class="font-bold border-b-2 mb-4 pb-2">Product description: </h3>
+        <p class="mb-7">{{ productDetails.description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,4 +31,8 @@ const { data: productDetails } = await useFetch(url, { key: id });
 console.log(productDetails);
 </script>
 
-<style scoped></style>
+<style scoped>
+img{
+  max-width: 400px;
+}
+</style>
